62f18426c1d9524b26fc50ca98a1dd52ca82ab73
Sunny Tamang <spikestamang@gmail.com>
2021-04-28 20:42:19 +0530

    with fragments

---
+++ app/src/main/java/com/example/covidtracker/FirstFrag.java
@@ -0,0 +1,25 @@
+package com.example.covidtracker;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FirstFrag extends Fragment {
+
+    View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_first,container,false);
+
+        return view;
+    }
+
+
+}
