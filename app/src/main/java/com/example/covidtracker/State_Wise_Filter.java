27b744b7a7ea6664f4748932db58d05fb0f05d56
Sunny Tamang <spikestamang@gmail.com>
2021-05-06 00:10:13 +0530

    statewise bg bottom curved

--- app/src/main/java/com/example/covidtracker/State_Wise_Filter.java
+++ app/src/main/java/com/example/covidtracker/State_Wise_Filter.java
@@ -1,5 +1,6 @@
 package com.example.covidtracker;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,32 +13,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 import java.util.ArrayList;
 
-public class State_Wise_Filter extends Fragment {
+class State_Wise_Filter extends Fragment {
 
 
 //    public State_Wise_Filter() {
 //        // Required empty public constructor
 //    }
 
-
-    // private ArrayList<ExampleItem> mExampleItem;
+    View view;
+   // private ArrayList<ExampleItem> mExampleItem;
     private RecyclerView mRecyclerView;
+    private PieChart pieChart;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_state__wise__filter, container, false);
-
+        createExampleList();
+        buildRecyclerView();
 
         EditText editText = view.findViewById(R.id.search_state);
         editText.addTextChangedListener(new TextWatcher() {
@@ -56,12 +59,30 @@ public class State_Wise_Filter extends Fragment {
                 filter(s.toString());
             }
         });
-        
-        return view;
+    }
+    private void filter(String text){
+       // ArrayList<ExampleItem>
+    }
+
+    private void createExampleList() {
 
     }
 
-    private void filter(String toString) {
+    private void buildRecyclerView() {
+
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_state__wise__filter, container, false);
+
+
+
+        return view;
     }
 
 
